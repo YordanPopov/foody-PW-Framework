d445640aa6a31fbbdf77faabd98aca835e8f4e8f
YordanPopov <jordanpopov190@gmail.com>
2025-09-27 21:48:54 +0300

    implement navigation bar page object class

---
+++ pages/clientSite/navigationBar.ts
@@ -0,0 +1,122 @@
+import { Page, Locator, expect } from '@playwright/test';
+/**
+ * This is the page object for the NavigationBar Page.
+ * @export
+ * @class NavigationBar
+ * @typedef {NavigationBar}
+ */
+export class NavigationBar {
+  constructor(private page: Page) {}
+
+  get homePageNavigationLink(): Locator {
+    return this.page.getByRole('link', {
+      name: 'FOODY',
+      exact: true,
+    });
+  }
+
+  get loginPageNavigationLink(): Locator {
+    return this.page.getByRole('link', {
+      name: 'Log In',
+      exact: true,
+    });
+  }
+
+  get signUpPageNavigationLink(): Locator {
+    return this.page.getByRole('link', {
+      name: 'Sign Up',
+      exact: true,
+    });
+  }
+
+  get addFoodNavigationLink(): Locator {
+    return this.page.getByRole('link', {
+      name: 'Add Food',
+      exact: true,
+    });
+  }
+
+  get logoutLink(): Locator {
+    return this.page.getByRole('link', {
+      name: 'Logout',
+      exact: true,
+    });
+  }
+
+  /**
+   * Navigate to the Login page
+   * @returns {Promise<void>} - Resolves when navigation to the login page is complete.
+   */
+  async openLoginPage(): Promise<void> {
+    await Promise.all([
+      this.page.waitForResponse(
+        (response) =>
+          response.url().includes('User/Login') && response.status() === 200
+      ),
+
+      this.loginPageNavigationLink.click(),
+    ]);
+
+    await expect(
+      this.page.getByRole('paragraph', {
+        name: 'Please login to your account',
+      })
+    ).toBeVisible();
+  }
+
+  /**
+   * Navigate to the SignUp page
+   * @returns {Promise<void>} - Resolves when navigation to the signup page is complete.
+   */
+  async openSignUpPage(): Promise<void> {
+    await Promise.all([
+      this.page.waitForResponse(
+        (response) =>
+          response.url().includes('User/Register') && response.status() === 200
+      ),
+
+      this.signUpPageNavigationLink.click(),
+    ]);
+
+    await expect(
+      this.page.getByRole('paragraph', {
+        name: 'Please register new account',
+      })
+    ).toBeVisible();
+  }
+
+  /**
+   * Navigate to the Add Food page
+   * @returns {Promise<void>} - Resolves when navigation to the add food page is complete.
+   */
+  async openAddFoodPage(): Promise<void> {
+    await Promise.all([
+      this.page.waitForResponse(
+        (response) =>
+          response.url().includes('Food/Add') && response.status() === 200
+      ),
+
+      this.addFoodNavigationLink.click(),
+    ]);
+
+    await expect(
+      this.page.getByRole('heading', {
+        name: 'Add your food here',
+      })
+    ).toBeVisible();
+  }
+
+  /**
+   * user logout
+   * @returns {Promise<void>} - Resolves when user is logged out.
+   */
+  async logout(): Promise<void> {
+    await this.logoutLink.click();
+
+    await expect(
+      this.page.getByRole('link', {
+        name: 'Learn More',
+      })
+    ).toBeVisible();
+  }
+}
