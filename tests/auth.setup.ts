d8b4b8520eb555779cf40d62801185977e7b7a36
YordanPopov <jordanpopov190@gmail.com>
2025-10-25 21:39:58 +0300

    implement setup for authentication user by api

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,13 +1,35 @@
-import { test as setup } from '../fixtures/pom/test-options';
+import { test as setup, expect } from '../fixtures/pom/test-options';
+import { AuthUserResponse } from '../fixtures/api/types-guards';
+import { authUserSchema } from '../fixtures/api/schemas';
 
-setup('auth user', async ({ loginPage, navBar, homePage, page }) => {
-  await setup.step('create logged in user session', async () => {
-    await homePage.gotoHomePage();
+setup(
+  'auth user',
+  async ({ loginPage, navBar, homePage, page, apiRequest }) => {
+    await setup.step('auth for user by API', async () => {
+      const { status, body } = await apiRequest<AuthUserResponse>({
+        method: 'POST',
+        baseUrl: process.env.API_URL,
+        url: 'User/Authentication',
+        body: {
+          userName: process.env.USER_NAME,
+          password: process.env.PASSWORD,
+        },
+      });
 
-    await navBar.openLoginPage();
+      expect(status).toBe(200);
+      expect(authUserSchema.parse(body)).toBeTruthy();
 
-    await loginPage.login(process.env.USER_NAME!, process.env.PASSWORD!);
+      process.env['ACCESS_TOKEN'] = body.accessToken;
+    });
 
-    await page.context().storageState({ path: '.auth/userSession.json' });
-  });
-});
+    await setup.step('create logged in user session', async () => {
+      await homePage.gotoHomePage();
+
+      await navBar.openLoginPage();
+
+      await loginPage.login(process.env.USER_NAME!, process.env.PASSWORD!);
+
+      await page.context().storageState({ path: '.auth/userSession.json' });
+    });
+  }
+);
