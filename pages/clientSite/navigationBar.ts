38483112ea4685a9cbb420fed2a38e0fba7e9319
YordanPopov <jordanpopov190@gmail.com>
2025-11-09 21:24:49 +0200

    refactor: use getter instead of fields

--- pages/clientSite/navigationBar.ts
+++ pages/clientSite/navigationBar.ts
@@ -1,35 +1,69 @@
 import { Page, Locator, expect } from '@playwright/test';
-import BasePage from './BasePage';
-import { setEngine } from 'crypto';
 /**
  * This is the page object for the NavigationBar Page.
  * @export
  * @class NavigationBar
  * @typedef {NavigationBar}
  */
-export default class NavigationBar extends BasePage {
-  constructor(page: Page) {
-    super(page);
+export class NavigationBar {
+  constructor(private page: Page) {}
+
+  get loginLink(): Locator {
+    return this.page.getByRole('link', {
+      name: 'Log In',
+    });
   }
 
-  private readonly selectors = {
-    homePageNavigationLink: 'a:has-text("FOODY")',
-    loginPageNavigationLink: 'a:has-text("Log In")',
-    signUpPageNavigationLink: 'a:has-text("Sign Up")',
-    addFoodNavigationLink: 'a.nav-link:has-text("Add Food")',
-    logoutLink: 'a:has-text("Logout")',
-  };
+  get signUpLink(): Locator {
+    return this.page.getByRole('link', {
+      name: 'Sign Up',
+    });
+  }
+
+  get homePageLink(): Locator {
+    return this.page.getByRole('link', {
+      name: 'FOODY',
+    });
+  }
+
+  get myProfilIcon(): Locator {
+    return this.page.locator('.nav-link.mx-3');
+  }
+
+  get addFoodLink(): Locator {
+    return this.page.getByRole('link', {
+      name: 'Add Food',
+    });
+  }
+
+  get logoutLink(): Locator {
+    return this.page.getByRole('link', {
+      name: 'Logout',
+    });
+  }
+
+  /**
+   * Navigate to the Home Page
+   * @returns {Promise<void>} - Resolves when navigation to the home page is complete.
+   */
+  async openHomePage(): Promise<void> {
+    await this.homePageLink.click();
+
+    await expect(this.page.locator('h1:has-text("FOODY")')).toBeVisible();
+  }
 
   /**
    * Navigate to the Login page
    * @returns {Promise<void>} - Resolves when navigation to the login page is complete.
    */
   async openLoginPage(): Promise<void> {
-    await this.clickElement(this.selectors.loginPageNavigationLink);
+    await this.loginLink.click();
 
-    await this.verifyElementVisible(
-      'p:has-text("Please login to your account")'
-    );
+    await expect(
+      this.page.getByText('Please login to your account', {
+        exact: true,
+      })
+    ).toBeVisible();
   }
 
   /**
@@ -37,21 +71,30 @@ export default class NavigationBar extends BasePage {
    * @returns {Promise<void>} - Resolves when navigation to the signup page is complete.
    */
   async openSignUpPage(): Promise<void> {
-    await this.clickElement(this.selectors.signUpPageNavigationLink);
+    await this.signUpLink.click();
 
-    await this.verifyElementVisible(
-      'p:has-text("Please register new account")'
-    );
+    await expect(
+      this.page.getByText('Please register new account', {
+        exact: true,
+      })
+    ).toBeVisible();
   }
-
   /**
    * Navigate to the Add Food page
    * @returns {Promise<void>} - Resolves when navigation to the add food page is complete.
    */
   async openAddFoodPage(): Promise<void> {
-    await this.clickElement(this.selectors.addFoodNavigationLink);
+    await this.addFoodLink.click();
 
-    await this.verifyElementVisible('h4.mt-1');
+    await expect(this.page.locator('h4.mt-1.mb-5.pb-1')).toBeVisible();
+  }
+
+  async openMyProfilePage(): Promise<void> {
+    await this.myProfilIcon.click();
+
+    await expect(
+      this.page.getByText('About me:', { exact: true })
+    ).toBeVisible();
   }
 
   /**
@@ -59,8 +102,12 @@ export default class NavigationBar extends BasePage {
    * @returns {Promise<void>} - Resolves when user is logged out.
    */
   async logout(): Promise<void> {
-    await this.clickElement(this.selectors.logoutLink);
+    await this.logoutLink.click();
 
-    await this.verifyElementVisible('a:has-text("Learn More")');
+    await expect(
+      this.page.getByText('Share your homemade or store-bought food with us!', {
+        exact: true,
+      })
+    ).toBeVisible();
   }
 }
